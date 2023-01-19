05284df40097019716328367720518c18a22eaa8
Cory Pollard <astandard87@gmail.com>
2023-01-18 21:52:29 -0600

    Added meal items along with mealitem form and input

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import classes from './AvailableMeals.module.css';
 import DUMMY_MEALS from '../../store/dummyData';
 import Card from '../UI/Card';
+import MealItem from './MealItems/MealItem';
 
 console.log(DUMMY_MEALS);
 
 const AvailableMeals = props => {
-  const meals = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>);
+  const meals = DUMMY_MEALS.map(meal => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+      id={meal.id}
+    />
+  ));
   return (
     <section className={classes.meals}>
       <Card>
