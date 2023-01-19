fee38face3fa9b50921b56df760a892a69642343
Cory Pollard <astandard87@gmail.com>
2023-01-19 12:04:24 -0600

    Created modal and portal to overlay root in html

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+
+const Backdrop = props => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = props => {
+  return <div className={classes.modal}>{props.children}</div>;
+};
+
+const parentElement = document.getElementById('overlay-root');
+
+const Modal = props => {
+  return (
+    <>
+      {ReactDOM.createPortal(Backdrop, parentElement)}
+      {ReactDOM.createPortal(ModalOverlay, parentElement)}
+    </>
+  );
+};
+
+export default Modal;
