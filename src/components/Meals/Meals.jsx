7bb523886d78794d788f27821fb6aa1001209acf
Cory Pollard <astandard87@gmail.com>
2023-01-18 21:17:04 -0600

    Created card, meals and meals summary sections

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MealsSummary from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <section>
+      <MealsSummary />
+    </section>
+  );
+};
+
+export default Meals;
